a843d328c7201b9e8a476d40bd58db220354e194
mckoyd <dominick.mckoy@gmail.com>
2018-06-26 09:25:25 -0400

    Added constructor for BinarySearchTree class

---
+++ BinarySearchTree.js
@@ -0,0 +1,9 @@
+class BinarySearchTree {
+  constructor(key=null, value=null, parent=null){
+    this.key = key;
+    this.value = value;
+    this.parent = parent;
+    this.left = null;
+    this.right = null;
+  }
+}
