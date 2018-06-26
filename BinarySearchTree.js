fde0ba4e4e27860b5ee8a9bf133f5ebcf4eaa3c2
mckoyd <dominick.mckoy@gmail.com>
2018-06-26 09:30:16 -0400

    Added insert function to BST class

--- BinarySearchTree.js
+++ BinarySearchTree.js
@@ -6,4 +6,19 @@ class BinarySearchTree {
     this.left = null;
     this.right = null;
   }
+
+  insert(key, value){
+    if(this.key === null){
+      this.key = key;
+      this.value = value;
+    } else if (key < this.key) {
+      if (this.left === null){
+        this.left = new BinarySearchTree(key, value, this);
+      } else { this.left.insert(key, value); }
+    } else {
+      if (this.right === null){
+        this.right = new BinarySearchTree(key, value, this);
+      } else { this.right.insert(key, value); }
+    }
+  }
 }
